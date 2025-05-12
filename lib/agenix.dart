a8f84005d3d2cb3963b562a40d2b0cbb5d9eb5d9
ahmadexe <muahmad710@gmail.com>
2025-05-12 20:15:14 +0500

    refac: local files renamed

--- lib/agenix.dart
+++ lib/agenix.dart
@@ -1,1 +1,19 @@
+// Agenix
 
+// Core Agent
+export 'src/agent/agent.dart';
+
+// LLM
+export 'src/llm/llm.dart';
+export 'src/llm/gemini.dart';
+
+// Memory
+export 'src/memory/data/agent_message.dart';
+export 'src/memory/data/conversation.dart';
+export 'src/memory/data/data_store.dart';
+export 'src/memory/data_sources/firebase.dart';
+
+// Tools
+export 'src/tools/tool.dart';
+export 'src/tools/param_spec.dart';
+export 'src/tools/tool_registry.dart';
