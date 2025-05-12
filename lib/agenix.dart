b1dc2d22620fe2ecc6a314557b8e9a1532a4d2b5
ahmadexe <muahmad710@gmail.com>
2025-05-13 01:01:05 +0500

    feat: hiding implementation from base app

--- lib/agenix.dart
+++ lib/agenix.dart
@@ -5,13 +5,11 @@ export 'src/agent/agent.dart';
 
 // LLM
 export 'src/llm/llm.dart';
-export 'src/llm/gemini.dart';
 
 // Memory
 export 'src/memory/data/agent_message.dart';
 export 'src/memory/data/conversation.dart';
 export 'src/memory/data/data_store.dart';
-export 'src/memory/data_sources/firebase.dart';
 
 // Tools
 export 'src/tools/tool.dart';
--- lib/src/llm/llm.dart
+++ lib/src/llm/llm.dart
@@ -1,10 +1,15 @@
 // All LLM classes should implement this interface
 // This allows for easy swapping of LLMs (e.g., Gemini, Claude, etc.) without changing the core logic of the agent's memory management.
 
+import 'package:agenix/src/llm/gemini.dart';
+
 abstract class LLM {
   Future<String> generate({
     required String prompt,
   });
 
   String get modelId;
+
+  // Add more methods as needed
+  static geminiLLM({required String apiKey, required String modelName}) => Gemini(apiKey: apiKey, modelName: modelName);
 }
