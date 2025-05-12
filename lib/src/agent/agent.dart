f67d0c6b3aad0907aff482b1b4feceb63def6b52
ahmadexe <muahmad710@gmail.com>
2025-05-12 20:06:20 +0500

    feat: tool use case is setup

--- lib/src/agent/agent.dart
+++ lib/src/agent/agent.dart
@@ -3,7 +3,10 @@ import 'dart:convert';
 import 'package:agenix/src/llm/llm.dart';
 import 'package:agenix/src/memory/data/agent_message.dart';
 import 'package:agenix/src/memory/data/data_store.dart';
+import 'package:agenix/src/static/_pkg_constants.dart';
+import 'package:agenix/src/tools/parser.dart';
 import 'package:agenix/src/tools/tool_registry.dart';
+import 'package:agenix/src/tools/tool_runner.dart';
 import 'package:flutter/services.dart';
 
 part '_memory_manager.dart';
@@ -15,10 +18,12 @@ class Agent {
   Agent._internal();
 
   bool _isInitialized = false;
-  
+
   late final _MemoryManager _memoryManager;
   late final _PromptBuilder _promptBuilder;
   late final LLM llm;
+  late final PromptParser _promptParser;
+  late final ToolRunner _toolRunner;
 
   Future<void> init({required DataStore dataStore, required LLM llm}) async {
     if (_isInitialized) {
@@ -33,6 +38,9 @@ class Agent {
     String jsonString = await rootBundle.loadString('assets/system_data.json');
     final raw = json.decode(jsonString);
     _promptBuilder = _PromptBuilder(systemPrompt: raw);
+
+    _promptParser = PromptParser();
+    _toolRunner = ToolRunner();
   }
 
   Future<String> generateResponse({
@@ -51,7 +59,22 @@ class Agent {
       userMessage: userMessage,
     );
 
-    final response = await llm.generate(prompt: prompt);
-    return response;
+    final rawLLMResponse = await llm.generate(prompt: prompt);
+
+    final parsed = _promptParser.parse(rawLLMResponse);
+
+    if (parsed.toolNames.isEmpty) {
+      return parsed.fallbackResponse ?? kLLMResponseOnFailure;
+    }
+
+    final toolResponses = await _toolRunner.runTools(parsed);
+
+    String response = toolResponses
+        .map((r) => r.values.first.toString())
+        .join("\n");
+    if (response.isEmpty) {
+      return parsed.fallbackResponse ?? kLLMResponseOnFailure;
+    }
+    return "This is the infromation I can find for your query\n$response";
   }
 }
